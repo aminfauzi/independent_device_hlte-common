1561a355fff69762faca1e477d80a79e2d78d444
Bruno Martins <bgcngm@gmail.com>
2018-01-26 11:27:35 +0000

    msm8974-common: Binderize them all
     * Switch to binderized HAL services as possible and update
       HIDL manifest accordingly.
    
    Change-Id: Id50291488d655187aa013c51bdd6890dca010564
    Signed-off-by: Joshua Primero <jprimero155@gmail.com>

--- msm8974.mk
+++ msm8974.mk
@@ -62,6 +62,7 @@ PRODUCT_PACKAGES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl \
+    android.hardware.bluetooth@1.0-service \
     libbt-vendor
 
 # Boot animation
@@ -74,6 +75,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
     android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service \
     copybit.msm8974 \
     gralloc.msm8974 \
     hwcomposer.msm8974 \
@@ -118,7 +120,8 @@ PRODUCT_COPY_FILES += \
 
 # Keymaster
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service
 
 # Lights
 PRODUCT_PACKAGES += \
