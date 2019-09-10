d752384f79213f3959d6ef224345136ea92959c6
Alessandro Astone <ales.astone@gmail.com>
2019-09-10 09:57:50 +0200

    hlte-common: Shim libperipheral_client.so
    * LocSvc_afw: get_geofence_interface, dlopen for libgeofence.so
      failed, error = dlopen failed: cannot locate symbol
      "_ZN7android10IInterface8asBinderEv" referenced by
      "/system/vendor/lib/libperipheral_client.so"...
    * vndksupport: Could not load /vendor/lib/hw/flp.default.so from
      default namespace: dlopen failed: cannot locate symbol
      "_ZN7android10IInterface8asBinderEv" referenced by
      "/system/vendor/lib/libperipheral_client.so"....
    
    Change-Id: I4fed5fc23b1721e75e9759c0811b7532c5b68705
    Signed-off-by: Joshua Primero <jprimero155@gmail.com>

--- hlte.mk
+++ hlte.mk
@@ -92,6 +92,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/_hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/_hals.conf
 
+# Shims
+PRODUCT_PACKAGES += \
+    libshim_binder
+
 # Shipping API lvl
 PRODUCT_SHIPPING_API_LEVEL := 18
 
