dd551d392666fd47766a4d4b91eb9e79f97afd6d
Joshua Primero <jprimero155@gmail.com>
2020-06-08 08:38:39 +0000

    hlte-common: Build ParanoidDoze
    Signed-off-by: Joshua Primero <jprimero155@gmail.com>

--- hlte.mk
+++ hlte.mk
@@ -85,6 +85,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
+# ParanoidDoze
+PRODUCT_PACKAGES += \
+    ParanoidDoze
+
 # Radio
 PRODUCT_PACKAGES += \
     libsecnativefeature
